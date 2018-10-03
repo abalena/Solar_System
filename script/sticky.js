21fcb0d04886f7356a589dbfe22a3f4f1e3a8ba7
abalena <alenaabakumenko@gmail.com>
2018-10-03 16:04:56 +0300

    redesign page, add sticky-header

---
+++ script/sticky.js
@@ -0,0 +1,10 @@
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 100) {
+        $('nav').addClass('fixed-header');
+        $('nav div').addClass('visible-title');
+    }
+    else {
+        $('nav').removeClass('fixed-header');
+        $('nav div').removeClass('visible-title');
+    }
+});
